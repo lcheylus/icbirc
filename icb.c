ce3bb64ba9764dcd651fe33869e223edca847e51
Laurent Cheylus <foxy@free.fr>
2023-10-23 14:58:50 +0200

    Update license notice for 2023
    Signed-off-by: Laurent Cheylus <foxy@free.fr>

--- icb.c
+++ icb.c
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2003-2004 Daniel Hartmeier
+ * Copyright (c) 2023 Laurent Cheylus <foxy@free.fr>
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
